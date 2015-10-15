9e7ec3f2002e2675fd4552133203cbd4e6349ca2
Anilkumar19 <anilkumarbellamkonda91@gmail.com>
2015-10-15 21:04:03 +0530

    ff

--- config/initializers/omniauth.rb
+++
@@ -1,6 +0,0 @@
-OmniAuth.config.logger = Rails.logger
-
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, '105083838432-cb5dgkksbooh2n68f47c61dogvnqojvh.apps.googleusercontent.com', 'dTPQlOltsKtYVpDO4tsvxrrv', {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
-  
-end
