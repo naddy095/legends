f69955f46dfa3555f80148036d7b4ce20eb57fda
Rajkumar <rajkumar.ulaganadhan@honeydew-technologies.com>
2015-10-15 16:21:32 +0530

    asnm

--- config/initializers/omniauth.rb
+++
@@ -1,7 +0,0 @@
-#config/initalizers/omniauth.rb
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, ENV['305327068123-d58if82b2vsff87momvvqonq44mccr7k.apps.googleusercontent.com'], ENV['fohN8AJn0WhrWXhQZdoUC5Yh'], {
-  scope: ['email',
-    'https://www.googleapis.com/auth/gmail.modify'],
-    access_type: 'offline'}
-end
